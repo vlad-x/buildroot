10a513679ef9d867ed49942a42a4d228a234619b
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-02-05 19:07:15 +0100

    polkit: bump version
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>

--- package/polkit/polkit.mk
+++ package/polkit/polkit.mk
@@ -4,20 +4,24 @@
 #
 ################################################################################
 
-POLKIT_VERSION = 0.103
+POLKIT_VERSION = 0.112
 POLKIT_SITE = http://www.freedesktop.org/software/polkit/releases/
 POLKIT_LICENSE = GPLv2
 POLKIT_LICENSE_FILES = COPYING
 
 POLKIT_INSTALL_STAGING = YES
 
-POLKIT_DEPENDENCIES = libglib2 host-intltool expat
+POLKIT_DEPENDENCIES = expat host-intltool host-pkgconf libglib2 spidermonkey
 
 # We could also support --with-authfw=pam
 POLKIT_CONF_OPT = \
 	--with-authfw=shadow \
 	--with-os-type=unknown \
 	--disable-man-pages \
-	--disable-examples
+	--disable-examples \
+	--with-mozjs=mozjs-24
+
+POLKIT_CONF_ENV = \
+	PKG_CONFIG_PATH="$(HOST_DIR)/usr/lib/pkgconfig:$(PKG_CONFIG_PATH)"
 
 $(eval $(autotools-package))
