8847d2cf0240dd6b39adcd76228004c4ed8c67b8
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-21 00:30:56 +0200

    geoip: bump version
    The tarball from the release contains the configure and Makefile.in,
    there's no longer need to AUTORECONF.
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>

--- package/geoip/geoip.mk
+++ package/geoip/geoip.mk
@@ -4,10 +4,8 @@
 #
 ################################################################################
 
-GEOIP_VERSION = 1.6.0
-GEOIP_SOURCE = GeoIP-$(GEOIP_VERSION).tar.gz
-GEOIP_SITE = $(call github,maxmind,geoip-api-c,v$(GEOIP_VERSION))
-GEOIP_AUTORECONF = YES
+GEOIP_VERSION = 1.6.2
+GEOIP_SITE = https://github.com/maxmind/geoip-api-c/releases/download/v$(GEOIP_VERSION)/
 GEOIP_INSTALL_STAGING = YES
 GEOIP_LICENSE = LGPLv2.1+
 GEOIP_LICENSE_FILES = LICENSE COPYING
