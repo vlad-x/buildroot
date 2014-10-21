514c168814f004653a4943125f33c3303f5d4738
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-22 00:21:19 +0200

    protobuf-c: bump version
    The releases contains generated Makefile and configure, there's no need
    to AUTORECONF anymore.
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>

--- package/protobuf-c/protobuf-c.mk
+++ package/protobuf-c/protobuf-c.mk
@@ -4,8 +4,8 @@
 #
 ################################################################################
 
-PROTOBUF_C_VERSION = v1.0.0-rc1
-PROTOBUF_C_SITE = $(call github,protobuf-c,protobuf-c,$(PROTOBUF_C_VERSION))
+PROTOBUF_C_VERSION = 1.0.2
+PROTOBUF_C_SITE = https://github.com/protobuf-c/protobuf-c/releases/download/v$(PROTOBUF_C_VERSION)
 PROTOBUF_C_DEPENDENCIES = host-protobuf-c
 HOST_PROTOBUF_C_DEPENDENCIES = host-protobuf host-pkgconf
 PROTOBUF_C_MAKE = $(MAKE1)
@@ -13,8 +13,6 @@ PROTOBUF_C_CONF_OPTS = --disable-protoc
 PROTOBUF_C_INSTALL_STAGING = YES
 PROTOBUF_C_LICENSE = BSD-2c
 PROTOBUF_C_LICENSE_FILES = LICENSE
-PROTOBUF_C_AUTORECONF = YES
-HOST_PROTOBUF_C_AUTORECONF = YES
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
