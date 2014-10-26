c672c21819afd137621b258ff4ba76b812b231a9
Maxime Hadjinlian <maxime.hadjinlian@gmail.com>
2014-10-26 14:13:07 +0100

    python-libconfig: bump version
    The patch will be sent to upstream, it is needed to avoid searching
    headers and library in the host machine.
    
    Signed-off-by: Maxime Hadjinlian <maxime.hadjinlian@gmail.com>

--- package/python-libconfig/python-libconfig.mk
+++ package/python-libconfig/python-libconfig.mk
@@ -4,14 +4,18 @@
 #
 ################################################################################
 
-PYTHON_LIBCONFIG_VERSION = b271c3d9dac938ad5cd29b67bd08cc5536a5a391
+PYTHON_LIBCONFIG_VERSION = 2f0c9803c98871ff28144f6001442ddcb2762933
 PYTHON_LIBCONFIG_SITE = $(call github,cnangel,python-libconfig,$(PYTHON_LIBCONFIG_VERSION))
-
 PYTHON_LIBCONFIG_LICENSE = BSD
 PYTHON_LIBCONFIG_LICENSE_FILES = README
-
 PYTHON_LIBCONFIG_SETUP_TYPE = setuptools
-
 PYTHON_LIBCONFIG_DEPENDENCIES = libconfig boost
+
+define PYTHON_LIBCONFIG_BUILD_CMDS
+    (cd $(@D); \
+        $(PKG_PYTHON_SETUPTOOLS_ENV) \
+        $(HOST_DIR)/usr/bin/python setup.py build_ext \
+            --boostpython=$(STAGING_DIR)/usr)
+endef
 
 $(eval $(python-package))
